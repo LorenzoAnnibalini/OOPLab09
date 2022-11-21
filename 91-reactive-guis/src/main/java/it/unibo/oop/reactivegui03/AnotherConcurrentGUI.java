d2c54ff7f4a9b02fba2516fcb9f46b497c71f37b
Lorenzo Annibalini - lorenzo.annibalini@studio.unibo.it <lorenzo.annibalini@studio.unibo.it>
2022-11-21 12:19:30 +0100

    Tolto import non utilizzati

--- 91-reactive-guis/src/main/java/it/unibo/oop/reactivegui03/AnotherConcurrentGUI.java
+++ 91-reactive-guis/src/main/java/it/unibo/oop/reactivegui03/AnotherConcurrentGUI.java
@@ -4,12 +4,9 @@ import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.lang.reflect.InvocationTargetException;
-import java.util.concurrent.TimeUnit;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
