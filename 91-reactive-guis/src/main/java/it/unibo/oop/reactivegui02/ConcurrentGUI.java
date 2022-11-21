5aad4ee122e2a4508cf72ca9aa3c1317444b235c
Lorenzo Annibalini - lorenzo.annibalini@studio.unibo.it <lorenzo.annibalini@studio.unibo.it>
2022-11-21 11:08:00 +0100

    Add gitattributes e Cominciato a finito grafica ConcurrentGUI

--- 91-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
+++ 91-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
@@ -7,10 +7,49 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
 
 /**
  * Second example of reactive GUI.
  */
 public final class ConcurrentGUI extends JFrame {
+
+    private final JFrame frame; 
+    private final JPanel p1;
+    private final JButton buttonUp;
+    private final JButton buttonDown;
+    private final JButton buttonStop;
+    private final JTextArea boxContatore;
+    private final Agent agent;
+
+    public ConcurrentGUI() {
+        this.frame = new JFrame();
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.agent = new Agent();
+        this.p1 = new JPanel();
+        this.buttonStop = new JButton("Stop");
+        this.buttonUp = new JButton("Up");
+        this.buttonDown = new JButton("Down");
+        this.boxContatore = new JTextArea();
+        this.boxContatore.setEditable(false);
+        this.frame.setContentPane(p1);
+        this.p1.add(boxContatore);
+        this.p1.add(buttonUp);
+        this.p1.add(buttonDown);
+        this.p1.add(buttonStop);
+
+        
+        //Rendo visibile frame
+        this.frame.pack();
+        this.frame.setVisible(true);
+    }
+
+    private class Agent implements Runnable {
+        @Override
+        public void run() {
+            // TODO Auto-generated method stub
+        }
+        
+    }
 }
